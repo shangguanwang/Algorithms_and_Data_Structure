2ad6d28a4759c957f9819873ba62912d6b7913cc
Cocoa Wang <wangshangguan@hotmail.com>
2023-04-02 11:58:47 -0500

    introduction to data structure queue

---
+++ 17-The-Queue/17_queue.js
@@ -0,0 +1,24 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+    // initialization
+    constructor() {
+        this.data = [];
+    }
+    // add method - add the record to the front
+    add(record) {
+        this.data.unshift(record);
+    }
+    // remove method - remove the last record
+    remove(record) {
+        return this.data.pop(record);
+    }
+}
---
+++ 17-The-Queue/17_the_queue_notes.md
@@ -0,0 +1,14 @@
+# Queue
+
+### Data Structures
+Definition: ways of organizing information with optimal runtime complexity for adding or removing records.
+
+### The Queue Data Structure
+
+record enters the container on one side and exit on the other side. It's like a waiting line of ticketing counter. First in first out: no cut in line.
+
+**Array equivalent method of a queue:**
+- Create a new empty queue: const q = new Queue();
+- Add to queue: array.unshift() | q.add(1);
+- Remove from queue: array.pop() | q.remove();
+
